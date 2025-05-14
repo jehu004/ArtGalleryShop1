34535f7a2bf86b5e86f75b3ee7a7650c07dce50b
Jehu004 <jensbhusem@gmail.com>
2025-05-14 14:00:54 +0200

    test

--- ArtGalleryShop1-jens-branch/backend/routes/Stripe.js
+++ ArtGalleryShop1-jens-branch/backend/routes/Stripe.js
@@ -48,11 +48,7 @@ router.post("/create-checkout-session", async (req, res) => {
       })),
       shipping_address_collection: {
         allowed_countries: [
-          "AL", "AD", "AM", "AT", "AZ", "BY", "BE", "BA", "BG", "HR", "CY", "CZ",
-          "DK", "EE", "FI", "FR", "GE", "DE", "GR", "HU", "IS", "IE", "IT", "KZ",
-          "XK", "LV", "LI", "LT", "LU", "MT", "MD", "MC", "ME", "NL", "MK", "NO",
-          "PL", "PT", "RO", "RU", "SM", "RS", "SK", "SI", "ES", "SE", "CH", "TR",
-          "UA", "GB", "VA"
+          "NO", "SE", "DK", "FR"
         ]
       },
 
